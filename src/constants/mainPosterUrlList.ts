ec8a319a9d106e6270209b9b349eb2f34d3344ed
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-06-21 06:25:44 +0900

    feat: 메인포스터 랜덤생성 함수 구현

---
+++ src/constants/mainPosterUrlList.ts
@@ -0,0 +1,13 @@
+export const mainPosterUrlList = [
+  "/CrwsWGdNtYpc1zGg1hPxuB88PT.jpg", //인디아나존스
+  "/CrwsWGdNtYpc1zGg1hPxuB88PT.jpg",
+  "/CrwsWGdNtYpc1zGg1hPxuB88PT.jpg",
+  "/CrwsWGdNtYpc1zGg1hPxuB88PT.jpg",
+  "/CrwsWGdNtYpc1zGg1hPxuB88PT.jpg",
+  "/7Yuf6j6N76SfqRWqT0I0ZUjICc2.jpg", //화염+ㅋㅋ
+  "/jv9WTuEk7kW8JxgpjQ872CyiifP.jpg", //커플
+  "/h8gHn0OzBoaefsYseUByqsmEDMY.jpg", //존웍
+  "/1inZm0xxXrpRfN0LxwE2TXzyLN6.jpg", //존웍2
+  "/1PQqvw7wnGeWqvXcBLaGK46n0zk.jpg", //캐릭터 1명
+  "/ioCuhiUs0SW5UgembG0UiQZWPLY.jpg", //슬픔이단체캐릭터
+];
