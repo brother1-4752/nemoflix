4e9cf9fba6609b1cb8c5cf3574eb3ea2de185267
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-06-20 23:58:18 +0900

    ♻️파일구조변경: App.tsx, main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RecoilRoot } from "recoil";
 import { RouterProvider } from "react-router-dom";
-
 import router from "./routes";
-import GlobalStyle from "./styles/GlobalStyle";
-
-const client = new QueryClient();
 
 export default function App() {
-  return (
-    <RecoilRoot>
-      <GlobalStyle />
-      <QueryClientProvider client={client}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </RecoilRoot>
-  );
+  return <RouterProvider router={router} />;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./styles/reset.css";
+import GlobalStyle from "./styles/GlobalStyle";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
