a8d9a42fe1599913711aa43b7c5fe06edef2102d
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-06-21 09:22:01 +0900

    debug: 하드코딩 상수값에 변수를 부여하여 더 명확하게 만듦.

--- src/hooks/useMoveToNextSlide.ts
+++ src/hooks/useMoveToNextSlide.ts
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export const useMoveToNextSlide = () => {
   const [cardDataIndex, setCardDataIndex] = useState(0);
+  const INITIAL_INDEX = 0;
+  const FINAL_INDEX = 18;
 
   const moveToNextSlide = (event: React.MouseEvent<HTMLButtonElement>) => {
     //TODO: undefined 가능성 있는 것들을 어떻게 처리해야할지 모르겠다
@@ -10,9 +12,8 @@ export const useMoveToNextSlide = () => {
       : -1;
     setCardDataIndex((prev) => {
       const calculatedIndex = prev + INDEX;
-      //TODO: 매직넘버 처리하기(0:INITIAL_INDEX, 18|19: FINAL_INDEX)
-      if (0 >= calculatedIndex) return 0;
-      else if (calculatedIndex >= 19) return 18;
+      if (INITIAL_INDEX >= calculatedIndex) return INITIAL_INDEX;
+      else if (calculatedIndex > FINAL_INDEX) return FINAL_INDEX;
       return prev + INDEX * 3;
     });
   };
